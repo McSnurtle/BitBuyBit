584b469d3b8082ce12eca937ee177da739f366e3
McSnurtle <93739492+McSnurtle@users.noreply.github.com>
2023-12-30 19:27:52 -0500

    Removed stupid GitHub format errors

--- main.py
+++ main.py
@@ -5,16 +5,7 @@ import os
 import time
 import sys
 import json
-<<<<<<< HEAD
-import random
-import threading
 import customtkinter as ctk
-import requests
-from tkinter import PhotoImage
-from PIL import Image, ImageTk
-=======
-import customtkinter as ctk
->>>>>>> 2b7be910f8dba4fa842ad7415db4844ba8062aa2
 from dotenv import load_dotenv
 
 from pages.meme import Meme
@@ -22,11 +13,6 @@ from pages.tickers import TickerSearch
 from pages.welcome import Welcome
 from pages.settings import Settings
 from pages.resources import ResourcesPage
-<<<<<<< HEAD
-from bars.fav_tickers import FavTickers
-from bars.navigation import NavigationBar
-from popup import CreateToolTip, Dialog, popup
-=======
 from pages.base_frame import BaseFrame, BaseScrollableFrame
 from bars.fav_tickers import FavTickers
 from bars.navigation import NavigationBar
@@ -38,7 +24,6 @@ import requests
 from tkinter import PhotoImage
 from PIL import Image, ImageTk
 from dotenv import load_dotenv
->>>>>>> 2b7be910f8dba4fa842ad7415db4844ba8062aa2
 
 import chart
 import exchange
@@ -69,30 +54,6 @@ class App(ctk.CTk):
 
         self.welcome_frame = Welcome(self, theme)
         self.welcome_frame.pack(side=ctk.BOTTOM, padx=theme['pad'][2], pady=theme['pad'][2], fill=ctk.BOTH, expand=True)
-<<<<<<< HEAD
-        self.meme_frame = Meme(master=self.welcome_frame, proj_dir=proj_dir, parent_geometry=(self.height * 0.5, self.height * 0.5))
-        self.meme_frame.pack(side=ctk.BOTTOM, padx=theme['pad'][2], pady=theme['pad'][2], fill=ctk.BOTH, expand=True)
-        self.ticker_frame = TickerSearch(self, theme, fav_tickers)
-        self.settings_frame = Settings(self, theme, fav_tickers)
-        self.resource_frame = ResourcesPage(self, theme)
-
-        # layout
-        pages = [('Tickers', lambda frame=self.ticker_frame: self.sel_frame(frame=frame)),
-                 ('Resources', lambda frame=self.resource_frame: self.sel_frame(frame=frame)),
-                 ('Settings', lambda frame=self.settings_frame: self.sel_frame(frame=frame))]
-        self.nav_bar = NavigationBar(self, pages, bg_color=theme['color']['bg2'], theme=theme)
-        self.nav_bar.pack(anchor=ctk.NW, side=ctk.TOP, fill=ctk.X)
-        if len(fav_tickers) > 0:
-            self.fav_tickers = FavTickers(
-                open_settings=lambda frame=self.settings_frame: self.sel_frame(frame=frame),
-                master=self)
-            self.fav_tickers.pack(anchor=ctk.NW, side=ctk.TOP, fill=ctk.X)
-
-        self.frames = [self.welcome_frame, self.ticker_frame, self.resource_frame, self.settings_frame]
-
-        self.update_time()
-
-=======
         self.meme_frame = Meme(master=self.welcome_frame, proj_dir=proj_dir,
                                parent_geometry=(self.height * 0.5, self.height * 0.5))
         self.meme_frame.pack(side=ctk.BOTTOM, padx=theme['pad'][2], pady=theme['pad'][2], fill=ctk.BOTH, expand=True)
@@ -102,11 +63,11 @@ class App(ctk.CTk):
 
         # layout
         pages_list = [('Tickers', lambda frame=self.ticker_frame: self.sel_frame(frame=frame)),
-                 ('Resources', lambda frame=self.resource_frame: self.sel_frame(frame=frame)),
-                 ('Settings', lambda frame=self.settings_frame: self.sel_frame(frame=frame))]
+                      ('Resources', lambda frame=self.resource_frame: self.sel_frame(frame=frame)),
+                      ('Settings', lambda frame=self.settings_frame: self.sel_frame(frame=frame))]
         self.nav_bar = NavigationBar(self, pages_list, bg_color=theme['color']['bg2'], theme=theme)
         self.nav_bar.pack(anchor=ctk.NW, side=ctk.TOP, fill=ctk.X)
-        
+
         if len(fav_tickers) > 0:
             self.fav_tickers = FavTickers(
                 open_settings=lambda frame=self.settings_frame: self.sel_frame(frame=frame),
@@ -124,7 +85,6 @@ class App(ctk.CTk):
         self.destroy()
         exit(0)
 
->>>>>>> 2b7be910f8dba4fa842ad7415db4844ba8062aa2
     def update_time(self):
         self.nav_bar.time_label.configure(text=f"{time.strftime('%H:%M:%S')}")
         self.nav_bar.time_label.after(1000, self.update_time)  # after 1 second...
